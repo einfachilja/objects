e9bbf24a397e6b33c431183e13b123e98ecb5559
Ilja Völker-Savkiv <ilja@MacBookAir.fritz.box>
2025-04-24 13:09:51 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+let myObjects = {
+    'name': 'Ilja',
+    'age': 34,
+    'job': {
+        'name': 'Digital Innovation Manager',
+        'company': 'stadtenergie',
+        'Probezeit': false,
+    },
+    'good guy': true
+};
+
+console.log(myObjects.job.name); // bessere Variante um auf Objetcs zuzugreifen
+
+console.log(myObjects['job']['name']); // weitere Variante um auf Objetcs zuzugreifen
+
+
+
+
+
