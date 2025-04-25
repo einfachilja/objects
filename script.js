dc3bd6d60173f0d9f4841d937eb01c99de169014
Ilja Savkiv <einfachilja@iMac.fritz.box>
2025-04-25 15:50:01 +0200

    added keys und entries

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ console.table([123, 4324, 123123, 4234, 123123]); // geht auch mit arrays
 */
 
 // 03 - Objects: methods
-
+/*
 // name der function ist vorne definiert
 let myObjects = {
     'name': 'Ilja', 
@@ -46,10 +46,29 @@ let myObjects = {
 myObjects.logJob(12);
 
 // console.table(myObjects)
+*/
+// 04 - Objects: keys und entries
 
+let myObjects = {
+  name: "Ilja",
+  age: 34,
+  job: "Dev",
+  good_guy: true,
+};
 
+// console.log(Object.keys(myObjects)); // Output Array mit den Keys
+// console.log(Object.entries(myObjects)); // Output Object mit Key: Value Pair und Arrays
 
+// Praxisbeispiel
 
+let objKeys = Object.keys(myObjects);
+console.log(objKeys); // Keys werden in einem Array ausgegeben
 
+let ourArray = [];
 
-
+for (let i = 0; i < objKeys.length; i++) {
+  // durch das Array objKeys gehen
+  const element = objKeys[i];
+  ourArray.push(myObjects[objKeys[i]]); // in das leere Array ourArray Werte z.B. Ilja pushen
+}
+console.log(ourArray);
