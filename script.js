0fcd816616ec59e92f683330c764e88744c4fb23
Ilja Savkiv <einfachilja@iMac.fritz.box>
2025-04-25 13:40:33 +0200

    create and access objects

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
+// 01 - Objects: Basics (Key-Value-Pair)
+
 let myObjects = {
-    'name': 'Ilja',
+    'name': 'Ilja', 
     'age': 34,
     'job': {
         'name': 'Digital Innovation Manager',
         'company': 'stadtenergie',
         'Probezeit': false,
     },
-    'good guy': true
+    'good_guy': true
 };
 
-console.log(myObjects.job.name); // bessere Variante um auf Objetcs zuzugreifen
+console.log(myObjects); // ganzes object ausgeloggt
+
+console.log(myObjects.job.name); // bessere Variante (Notation) um auf Objetcs zuzugreifen
 
 console.log(myObjects['job']['name']); // weitere Variante um auf Objetcs zuzugreifen
+
+let objKey = 'age';
+
+console.log(myObjects[objKey]); // Vorteil bei der Methode, man kann auch Variablen reingeben
+
+
+
 
 
 
