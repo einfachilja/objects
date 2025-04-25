5d9f9391b3f3cf5526eea3c184f91a4b7b5ac258
Ilja Savkiv <einfachilja@iMac.fritz.box>
2025-04-25 14:10:33 +0200

    added object methods

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // 01 - Objects: Basics (Key-Value-Pair)
-
+/*
 let myObjects = {
     'name': 'Ilja', 
     'age': 34,
@@ -20,7 +20,32 @@ console.log(myObjects['job']['name']); // weitere Variante um auf Objetcs zuzugr
 let objKey = 'age';
 
 console.log(myObjects[objKey]); // Vorteil bei der Methode, man kann auch Variablen reingeben
+*/
 
+// 02 - Objects: console.table
+/*
+console.log(myObjects); // ganzes object ausgeloggt
+
+console.table(myObjects); // cooler anzeigen lassen in Tabelle
+
+console.table([123, 4324, 123123, 4234, 123123]); // geht auch mit arrays
+*/
+
+// 03 - Objects: methods
+
+// name der function ist vorne definiert
+let myObjects = {
+    'name': 'Ilja', 
+    'age': 34,
+    'logJob': function (number) {
+        console.log('Dev-Mentor ' + number);  
+    },
+    'good_guy': true
+};
+
+myObjects.logJob(12);
+
+// console.table(myObjects)
 
 
 
